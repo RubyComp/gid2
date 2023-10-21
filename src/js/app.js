b49529dae949efd93289813297760d16d31491d1
RubyComp <alex61225@gmail.com>
2023-10-21 15:58:30 +0300

    common pages

--- src/js/app.js
+++ src/js/app.js
@@ -4,45 +4,44 @@ document.addEventListener('DOMContentLoaded', function () {
 	////////////////////////////////
 	// Accordion
 
-	// const sections = document.querySelectorAll('.accordion__section')
+	const sections = document.querySelectorAll('.accordion__section')
 
-	// if (sections) {
+	if (sections) {
 
-	// 	if (sections.length > 0) {
-	// 		sections[0].classList.add('active')
-	// 		const firstAnswer = sections[0].querySelector('.accordion__answer')
-	// 		firstAnswer.style.maxHeight = firstAnswer.scrollHeight + 'px'
-	// 	}
+		if (sections.length > 0) {
+			sections[0].classList.add('active')
+			const firstAnswer = sections[0].querySelector('.accordion__answer')
+			firstAnswer.style.maxHeight = firstAnswer.scrollHeight + 'px'
+		}
 
-	// 	function collapseAllExceptActive(activeSection) {
-	// 		sections.forEach((section) => {
-	// 				if (section !== activeSection) {
-	// 					section.classList.remove('active')
-	// 					section.querySelector('.accordion__answer').style.maxHeight = null
-	// 				}
-	// 		})
-	// 	}
+		function collapseAllExceptActive(activeSection) {
+			sections.forEach((section) => {
+					if (section !== activeSection) {
+						section.classList.remove('active')
+						section.querySelector('.accordion__answer').style.maxHeight = null
+					}
+			})
+		}
 
-	// 	function toggleAccordion() {
-	// 		const answer = this.nextElementSibling
-	// 		const isActive = this.parentNode.classList.contains('active')
+		function toggleAccordion() {
+			const answer = this.nextElementSibling
+			const isActive = this.parentNode.classList.contains('active')
 		
-	// 		collapseAllExceptActive(this.parentNode)
+			collapseAllExceptActive(this.parentNode)
 		
-	// 		if (isActive) {
-	// 				this.parentNode.classList.remove('active')
-	// 				answer.style.maxHeight = null
-	// 		} else {
-	// 				this.parentNode.classList.add('active')
-	// 				answer.style.maxHeight = answer.scrollHeight + 'px'
-	// 		}
-	// 	}
+			if (isActive) {
+					this.parentNode.classList.remove('active')
+					answer.style.maxHeight = null
+			} else {
+					this.parentNode.classList.add('active')
+					answer.style.maxHeight = answer.scrollHeight + 'px'
+			}
+		}
 
-	// 	sections.forEach((section) => {
-	// 		const question = section.querySelector('.accordion__question')
-	// 		question.addEventListener('click', toggleAccordion)
-	// 	})
-	// }
-	
+		sections.forEach((section) => {
+			const question = section.querySelector('.accordion__question')
+			question.addEventListener('click', toggleAccordion)
+		})
+	}
 
 })
